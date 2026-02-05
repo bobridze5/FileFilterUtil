7f8c54ca5174382d699c08dbafd7f88a662c42d8
Alsaev Dmitry <dima101203@gmail.com>
2026-02-05 22:01:35 +0700

    add README.md & some changes for gradle settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,39 @@
 plugins {
     id("java")
+    id("application")
     id("com.gradleup.shadow") version "9.3.1"
 }
 
 group = "ctf.alsaev"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass.set("ctf.alsaev.Main")
+}
+
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("com.beust:jcommander:1.82")
 }
 
-tasks.jar {
-    manifest {
-        attributes["Main-Class"] = "ctf.alsaev.Main"
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
-tasks.shadowJar {
-    archiveFileName.set("app.jar")
+tasks.jar {
+    enabled = false
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.shadowJar {
+    archiveFileName.set("util.jar")
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
